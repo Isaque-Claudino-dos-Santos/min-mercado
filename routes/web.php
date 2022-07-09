b31681b30ab235701fc735d952fd127063c07bfa
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-08 23:27:40 -0300

    router of produtos create,index,store

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,10 @@
 <?php
 
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
+Route::controller(ProdutosController::class)->group(function () {
+    Route::get('/produtos', 'index')->name('produtos.index');
+    Route::get('/produtos/create', 'create')->name('produtos.create');
+    Route::post('/produtos', 'store')->name('produtos.store');
+});
